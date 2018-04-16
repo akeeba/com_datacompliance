d78d6f35e9504fee7205a0880ca5919eab4f8e98
Davide Tampellini <tampe125@gmail.com>
2018-04-16 11:36:15 +0200

    Repository setup

---
+++ build/templates/link.php
@@ -0,0 +1,10 @@
+<?php
+// Internal linking script
+$hardlink_files = array();
+
+$symlink_files = [];
+
+$symlink_folders = array(
+	// Translations
+	//'../translations/admintools' => 'translations',
+);
---
+++ build/templates/version.php
@@ -0,0 +1,6 @@
+<?php
+// Protect from unauthorized access
+defined('_JEXEC') or die;
+
+define('AKCONNECTION_VERSION', '##VERSION##');
+define('AKCONNECTION_DATE', '##DATE##');
