02a0bc41e09b209b352e1e6f15fbc4186a122c81
Davide Tampellini <tampe125@gmail.com>
2018-04-17 16:20:25 +0200

    I'll add the backend later

--- component/backend/View/ControlPanel/tmpl/default.php
+++ component/backend/View/ControlPanel/tmpl/default.php
@@ -20,36 +20,6 @@ defined('_JEXEC') or die;
 		</div>
 
 		<div>
-            <div class="akeeba-panel--default">
-                <header class="akeeba-block-header">
-				    <h3><?php echo \JText::_('COM_ADMINTOOLS_LBL_CONTROLPANEL_UPDATES'); ?></h3>
-                </header>
-
-				<div>
-					<p>
-						Admin Tools version <?php echo AKCONNECTION_VERSION; ?> &bull;
-						<a href="#" id="btnAdminToolsChangelog" class="akeeba-btn--primary--small">CHANGELOG</a>
-					</p>
-
-					<p>Copyright &copy; 2018&ndash;<?php echo date('Y'); ?> Nicholas K. Dionysopoulos / <a
-								href="https://www.akeebabackup.com">Akeeba Ltd</a></p>
-				</div>
-
-                <div id="akeeba-changelog" tabindex="-1" role="dialog" aria-hidden="true" style="display:none;">
-                    <div class="akeeba-renderer-fof">
-                        <div class="akeeba-panel--info">
-                            <header class="akeeba-block-header">
-                                <h3>
-						            <?php echo \JText::_('CHANGELOG'); ?>
-                                </h3>
-                            </header>
-                            <div id="DialogBody">
-					            <?php echo $this->formattedChangelog; ?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
 		</div>
 	</div>
