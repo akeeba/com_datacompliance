8bbe907941186dc6ddf46de22fce286d331b66bb
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-04-21 12:00:19 +0300

    Add installation script to the plugin

---
+++ plugins/datacompliance/joomla/script.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package   Akeeba Data Compliance
+ * @copyright Copyright (c)2018 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+defined('_JEXEC') or die;
+
+// Load FOF if not already loaded
+if (!defined('FOF30_INCLUDED') && !@include_once(JPATH_LIBRARIES . '/fof30/include.php'))
+{
+	throw new RuntimeException('This extension requires FOF 3.0.');
+}
+
+class plgDatacomplianceJoomlaInstallerScript extends FOF30\Utils\InstallScript\Plugin
+{
+}
