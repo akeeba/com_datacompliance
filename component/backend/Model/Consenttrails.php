ee71ddf7f330fb91dafb2578e00dd5f81a308663
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-04-25 16:59:23 +0300

    gh-7 Mandatory custom user field to provide consent
    Render a Joomla article in the protection page when clicking "Learn more", configurable in the backend.

---
+++ component/backend/Model/Consenttrails.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @package   Akeeba Data Compliance
+ * @copyright Copyright (c)2018 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+
+namespace Akeeba\DataCompliance\Admin\Model;
+
+defined('_JEXEC') or die;
+
+use FOF30\Model\DataModel;
+use FOF30\Utils\Ip;
+
+/**
+ * Consent audit trails
+ *
+ * @property   string  $created_on    When the consent was given / revoked
+ * @property   int     $created_by    User consenting / revoking their consent
+ * @property   string  $requester_ip  The IP of the person who requested the export
+ * @property   int     $enabled       Was consent given?
+ */
+class Consenttrails extends DataModel
+{
+	/**
+	 * Checks the validity of the record. Also auto-fills the created* and requester_ip fields.
+	 *
+	 * @return  static
+	 */
+	public function check()
+	{
+		if (empty($this->requester_ip))
+		{
+			if ($this->container->platform->isCli())
+			{
+				$this->requester_ip = '(CLI)';
+			}
+			else
+			{
+				$this->requester_ip = Ip::getIp();
+			}
+		}
+
+		/** @var self $static This docblock is to keep phpStorm's static analysis from complaining */
+		$static = parent::check();
+
+		return $static;
+	}
+}
---
+++ component/frontend/View/Options/Html.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package   Akeeba Data Compliance
+ * @copyright Copyright (c)2018 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\DataCompliance\Site\View\Options;
+
+defined('_JEXEC') or die();
+
+use FOF30\View\DataView\Html as HtmlView;
+
+class Html extends HtmlView
+{
+
+}
