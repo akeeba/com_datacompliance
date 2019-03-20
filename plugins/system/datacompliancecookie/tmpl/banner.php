4bb2352b5c95ffee6cff292dda5c7c62f7d1d64a
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-03-20 13:55:45 +0200

    ~ Remove the flash of the cookie plugin popup once you've accepted cookies

--- plugins/system/datacompliancecookie/tmpl/banner.php
+++ plugins/system/datacompliancecookie/tmpl/banner.php
@@ -11,48 +11,49 @@
 defined('_JEXEC') or die;
 
 $position = $this->params->get('bannerPosition', 'top');
+$style = $this->hasCookiePreference && $this->hasAcceptedCookies ? 'style="display: none"' : '';
 ?>
-<div id="akeeba-dccc-banner-container" class="akeeba-renderer-fef akeeba-dccc-banner-<?php echo $position ?>">
+<div id="akeeba-dccc-banner-container" class="akeeba-renderer-fef akeeba-dccc-banner-<?= $position ?>" <?=$style?>>
 	<div class="akeeba-panel--primary">
 		<header class="akeeba-block-header">
 			<h2 class="akeeba-dccc-banner-header">
 				<span class="akion-alert-circled"></span>
-				<?php echo JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_HEADER') ?>
+				<?= JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_HEADER') ?>
 			</h2>
 		</header>
 
 		<p class="akeeba-dccc-banner-text">
-			<?php echo  JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_TEXT') ?>
+			<?=  JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_TEXT') ?>
 		</p>
 
 		<div id="akeeba-dccc-banner-controls">
 			<button onclick="window.AkeebaDataComplianceCookies.applyCookiePreference(0); return false;" class="akeeba-btn--red">
 				<span class="akion-ios-close-outline"></span>
-				<?php echo JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_BTN_DECLINE') ?>
+				<?= JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_BTN_DECLINE') ?>
 			</button>
 			<button onclick="window.AkeebaDataComplianceCookies.applyCookiePreference(1); return false;" class="akeeba-btn--green">
 				<span class="akion-ios-checkmark-outline"></span>
-				<?php echo JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_BTN_AGREE') ?>
+				<?= JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_BTN_AGREE') ?>
 			</button>
 		</div>
 
 		<ul class="akeeba-dccc-banner-links">
 			<li>
-				<a href="<?php echo $this->params->get('privacyPolicyURL', '') ?>"
+				<a href="<?= $this->params->get('privacyPolicyURL', '') ?>"
 				   class="akeeba-dccc-banner-link-privacypolicy">
-					<?php echo JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_LBL_PRIVACYPOLICY') ?>
+					<?= JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_LBL_PRIVACYPOLICY') ?>
 				</a>
 			</li>
 			<li>
 				<a href="https://eur-lex.europa.eu/LexUriServ/LexUriServ.do?uri=CELEX:32002L0058:EN:NOT"
 					class="akeeba-dccc-banner-link-privacypolicy-eprivacy" target="_blank">
-					<?php echo JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_LBL_EPRIVACY') ?>
+					<?= JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_LBL_EPRIVACY') ?>
 				</a>
 			</li>
 			<li>
 				<a href="https://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=CELEX:32016R0679"
 					class="akeeba-dccc-banner-link-gdpr" target="_blank">
-					<?php echo JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_LBL_GDPR') ?>
+					<?= JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_LBL_GDPR') ?>
 				</a>
 			</li>
 		</ul>
