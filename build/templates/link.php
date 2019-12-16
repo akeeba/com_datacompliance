319ede9cc18785d1ba6d0244176d5fac3ecb5090
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2019-12-16 11:13:59 +0200

    Update link.php after updating the FEF repo

--- build/templates/link.php
+++ build/templates/link.php
@@ -10,5 +10,5 @@ $hardlink_files  = [];
 $symlink_files   = [];
 $symlink_folders = [
 	// Force phpStorm to auto-complete CSS classes
-	'../fef/packages/joomla/fef/css/style.min.css' => 'fef.min.css',
+	'../fef/out/css/fef-joomla.min.css' => 'fef.min.css',
 ];
