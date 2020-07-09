d99c0dd7039e0767de53bc0116b46a299ace429d
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2020-07-09 16:06:42 +0300

    ~ Banner links to EU sites should have rel="noopener"

--- plugins/system/datacompliancecookie/tmpl/banner.php
+++ plugins/system/datacompliancecookie/tmpl/banner.php
@@ -5,20 +5,20 @@
  * @license   GNU General Public License version 3, or later
  */
 
-/** @var  PlgSystemDatacompliancecookie  $this */
+/** @var  PlgSystemDatacompliancecookie $this */
 
 // Prevent direct access
 defined('_JEXEC') or die;
 
 $position = $this->params->get('bannerPosition', 'top');
-$style = $this->hasCookiePreference && $this->hasAcceptedCookies ? 'style="display: none"' : '';
+$style    = $this->hasCookiePreference && $this->hasAcceptedCookies ? 'style="display: none"' : '';
 
 if ($this->hasCookiePreference)
 {
 	$style = 'style="display: none"';
 }
 ?>
-<div id="akeeba-dccc-banner-container" class="akeeba-renderer-fef akeeba-dccc-banner-<?= $position ?>" <?=$style?>>
+<div id="akeeba-dccc-banner-container" class="akeeba-renderer-fef akeeba-dccc-banner-<?= $position ?>" <?= $style ?>>
 	<div class="akeeba-panel--primary">
 		<header class="akeeba-block-header">
 			<h2 class="akeeba-dccc-banner-header">
@@ -28,15 +28,17 @@ if ($this->hasCookiePreference)
 		</header>
 
 		<p class="akeeba-dccc-banner-text">
-			<?=  JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_TEXT') ?>
+			<?= JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_TEXT') ?>
 		</p>
 
 		<div id="akeeba-dccc-banner-controls">
-			<button onclick="window.AkeebaDataComplianceCookies.applyCookiePreference(0); return false;" class="akeeba-btn--red">
+			<button onclick="window.AkeebaDataComplianceCookies.applyCookiePreference(0); return false;"
+					class="akeeba-btn--red">
 				<span class="akion-ios-close-outline"></span>
 				<?= JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_BTN_DECLINE') ?>
 			</button>
-			<button onclick="window.AkeebaDataComplianceCookies.applyCookiePreference(1); return false;" class="akeeba-btn--green">
+			<button onclick="window.AkeebaDataComplianceCookies.applyCookiePreference(1); return false;"
+					class="akeeba-btn--green">
 				<span class="akion-ios-checkmark-outline"></span>
 				<?= JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_BTN_AGREE') ?>
 			</button>
@@ -51,13 +53,15 @@ if ($this->hasCookiePreference)
 			</li>
 			<li>
 				<a href="https://eur-lex.europa.eu/LexUriServ/LexUriServ.do?uri=CELEX:32002L0058:EN:NOT"
-					class="akeeba-dccc-banner-link-privacypolicy-eprivacy" target="_blank">
+				   class="akeeba-dccc-banner-link-privacypolicy-eprivacy"
+				   target="_blank" rel="noopener">
 					<?= JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_LBL_EPRIVACY') ?>
 				</a>
 			</li>
 			<li>
 				<a href="https://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=CELEX:32016R0679"
-					class="akeeba-dccc-banner-link-gdpr" target="_blank">
+				   class="akeeba-dccc-banner-link-gdpr"
+				   target="_blank" rel="noopener">
 					<?= JText::_('PLG_SYSTEM_DATACOMPLIANCECOOKIE_BANNER_LBL_GDPR') ?>
 				</a>
 			</li>
