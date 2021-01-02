c7b9b2d78ed00eb1b7bafd2cd0b1d9a83a6e6383
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-01-02 12:38:19 +0200

    Updating copyright. Happy New Year!

--- component/backend/View/fef.php
+++ component/backend/View/fef.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package   ats
- * @copyright Copyright (c)2011-2020 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @copyright Copyright (c)2011-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
  * @license   GNU General Public License version 3, or later
  */
 
