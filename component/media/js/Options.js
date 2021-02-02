e2f8f1a9d4d63c2667337305229bdd9a9cc4d22d
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-02-02 14:53:55 +0200

    + Now using FEF 2 with a common JavaScript library across all Akeeba extensions
    I am not sure what got committed here.

--- component/media/js/Options.js
+++ component/media/js/Options.js
@@ -4,23 +4,26 @@
  * @license   GNU General Public License version 3, or later
  */
 
-akeeba.Loader.add(['akeeba.System'], function () {
-   akeeba.System.iterateNodes('a.akeebaDataComplianceArticleToggle', function (element) {
-       console.log(element);       akeeba.System.addEventHandler(element, "click", function (event) {
-           event.preventDefault();
+akeeba.Loader.add(["akeeba.System"], function ()
+{
+    akeeba.System.iterateNodes("a.akeebaDataComplianceArticleToggle", function (element)
+    {
+        akeeba.System.addEventListener(element, "click", function (event)
+        {
+            event.preventDefault();
 
-           var elTarget = event.currentTarget;
+            var elTarget = event.currentTarget;
 
-           if (elTarget.style.display === 'none')
-           {
-               elTarget.style.display = 'block';
+            if (elTarget.style.display === "none")
+            {
+                elTarget.style.display = "block";
 
-               return false;
-           }
+                return false;
+            }
 
-           elTarget.style.display = 'none';
+            elTarget.style.display = "none";
 
-           return false;
-       })
-   });
+            return false;
+        })
+    });
 });
