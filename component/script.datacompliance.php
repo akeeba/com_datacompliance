c77e78d4d47c926e88f63a003a7a56c48383b7f7
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-09-04 11:56:33 +0300

    Upgrade model and installation script

---
+++ component/script.datacompliance.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * @package   AkeebaDataCompliance
+ * @copyright Copyright (c)2018-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+defined('_JEXEC') or die;
+
+use Akeeba\Component\DataCompliance\Administrator\Helper\TemplateEmails;
+use Akeeba\Component\DataCompliance\Administrator\Model\UpgradeModel;
+use Joomla\CMS\Installer\Adapter\PackageAdapter;
+
+/**
+ * Akeeba Data Compliance package extension installation script file.
+ *
+ * @see https://docs.joomla.org/Manifest_files#Script_file
+ * @see UpgradeModel
+ */
+class Pkg_DatacomplianceInstallerScript
+{
+	/**
+	 * Called after any type of installation / uninstallation action.
+	 *
+	 * @param   string          $type    Which action is happening (install|uninstall|discover_install|update)
+	 * @param   PackageAdapter  $parent  The object responsible for running this script
+	 *
+	 * @return  bool
+	 * @since   1.0.0
+	 */
+	public function postflight(string $type, PackageAdapter $parent): bool
+	{
+		// Do not run on uninstall.
+		if ($type === 'uninstall')
+		{
+			return true;
+		}
+
+		$model = $this->getUpgradeModel();
+
+		if (empty($model))
+		{
+			return true;
+		}
+
+		if (!empty($model))
+		{
+			try
+			{
+				if (!$model->postflight($type, $parent))
+				{
+					return false;
+				}
+			}
+			catch (Exception $e)
+			{
+				return false;
+			}
+		}
+
+		$this->updateEmails();
+
+		return true;
+	}
+
+	/**
+	 * Get the UpgradeModel of the installed component
+	 *
+	 * @return  UpgradeModel|null  The upgrade Model. NULL if it cannot be loaded.
+	 * @since   1.0.0
+	 */
+	private function getUpgradeModel(): ?UpgradeModel
+	{
+		// Make sure the latest version of the Model file will be loaded, regardless of the OPcache state.
+		$filePath = JPATH_ADMINISTRATOR . '/components/com_datacompliance/src/Model/UpgradeModel.php';
+
+		if (function_exists('opcache_invalidate'))
+		{
+			opcache_invalidate($filePath = JPATH_ADMINISTRATOR . '/components/com_datacompliance/src/Model/UpgradeModel.php');
+		}
+
+		// Can I please load the model?
+		if (!class_exists('\Akeeba\Component\DataCompliance\Administrator\Model\UpgradeModel'))
+		{
+			if (!file_exists($filePath) || !is_readable($filePath))
+			{
+				return null;
+			}
+
+			include_once $filePath;
+		}
+
+		if (!class_exists('\Akeeba\Component\DataCompliance\Administrator\Model\UpgradeModel'))
+		{
+			return null;
+		}
+
+		try
+		{
+			return new UpgradeModel();
+		}
+		catch (Throwable $e)
+		{
+			return null;
+		}
+	}
+
+	private function updateEmails(): void
+	{
+		// Make sure the latest version of the Helper file will be loaded, regardless of the OPcache state.
+		$filePath = JPATH_ADMINISTRATOR . '/components/com_datacompliance/src/Helper/TemplateEmails.php';
+
+		if (function_exists('opcache_invalidate'))
+		{
+			opcache_invalidate($filePath);
+		}
+
+		if (!class_exists('\Akeeba\Component\DataCompliance\Administrator\Helper\TemplateEmails'))
+		{
+			if (!file_exists($filePath) || !is_readable($filePath))
+			{
+				return;
+			}
+
+			/** @noinspection PhpIncludeInspection */
+			include_once $filePath;
+		}
+
+		if (!class_exists('\Akeeba\Component\DataCompliance\Administrator\Helper\TemplateEmails'))
+		{
+			return;
+		}
+
+		try
+		{
+			TemplateEmails::updateAllTemplates();
+		}
+		catch (Exception $e)
+		{
+		}
+	}
+
+}
