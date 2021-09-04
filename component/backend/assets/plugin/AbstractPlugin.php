219bdc1966093e5cef6a71fcecba897410c4309d
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-09-04 11:46:26 +0300

    Abstract plugin and legacy shim

---
+++ component/backend/assets/plugin/AbstractPlugin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package   AkeebaDataCompliance
+ * @copyright Copyright (c)2018-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+defined('_JEXEC') or die;
+
+use Akeeba\Component\DataCompliance\Administrator\AbstractPlugin;
+
+/**
+ * Legacy shim to the abstract Data Compliance plugin class.
+ *
+ * @deprecated 4.0
+ */
+abstract class plgDatacomplianceAbstractPlugin extends AbstractPlugin
+{
+
+}
---
+++ component/backend/src/AbstractPlugin.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * @package   AkeebaDataCompliance
+ * @copyright Copyright (c)2018-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\Component\DataCompliance\Administrator;
+
+defined('_JEXEC') or die;
+
+use Joomla\Application\AbstractApplication;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\Database\DatabaseDriver;
+use Joomla\Event\DispatcherInterface;
+
+/**
+ * Common class for all DataCompliance plugins
+ *
+ * @since  3.0.0
+ */
+abstract class AbstractPlugin extends CMSPlugin
+{
+	/** @var AbstractApplication */
+	protected $app;
+
+	/** @var DatabaseDriver */
+	protected $db;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   DispatcherInterface  $subject  Passed by Joomla
+	 * @param   array                $config   Passed by Joomla
+	 *
+	 * @since   3.0.0
+	 */
+	public function __construct(DispatcherInterface &$subject, array $config = [])
+	{
+		$this->autoloadLanguage = true;
+
+		parent::__construct($subject, $config);
+	}
+
+	/**
+	 * Formats a number of bytes in human readable format
+	 *
+	 * @param   int  $size  The size in bytes to format, e.g. 8254862
+	 *
+	 * @return  string  The human-readable representation of the byte size, e.g. "7.87 Mb"
+	 *
+	 * @since   3.0.0
+	 */
+	protected function formatByteSize(int $size): string
+	{
+		$unit = ['b', 'Kb', 'Mb', 'Gb', 'Tb', 'Pb'];
+
+		return @round($size / pow(1024, ($i = floor(log($size, 1024)))), 2) . ' ' . $unit[$i];
+	}
+
+	/**
+	 * Returns the current memory usage, formatted human readable
+	 *
+	 * @return  string
+	 */
+	protected function memUsage(): string
+	{
+		if (function_exists('memory_get_usage'))
+		{
+			$size = memory_get_usage();
+
+			return $this->formatByteSize($size);
+		}
+		else
+		{
+			return "(unknown)";
+		}
+	}
+}
