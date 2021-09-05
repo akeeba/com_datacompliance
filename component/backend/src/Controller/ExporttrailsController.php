fa6d876896dfb5522b49c3c70739aec30f02f1a9
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-09-05 12:17:13 +0300

    Data export audit trail

---
+++ component/backend/src/Controller/ExporttrailsController.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package   AkeebaDataCompliance
+ * @copyright Copyright (c)2018-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+namespace Akeeba\Component\DataCompliance\Administrator\Controller;
+
+defined('_JEXEC') or die;
+
+use Akeeba\Component\DataCompliance\Administrator\Controller\Mixin\ControllerEvents;
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\MVC\Controller\AdminController;
+use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
+use Joomla\Input\Input;
+
+class ExporttrailsController extends AdminController
+{
+	use ControllerEvents;
+
+	protected $text_prefix = 'COM_DATACOMPLIANCE_EXPORTTRAILS';
+
+	public function __construct($config = [], MVCFactoryInterface $factory = null, ?CMSApplication $app = null, ?Input $input = null)
+	{
+		parent::__construct($config, $factory, $app, $input);
+
+		// This is a strictly browse-only view. We must not make changes to the audit trail!
+		$this->unregisterTask('unpublish');
+		$this->unregisterTask('archive');
+		$this->unregisterTask('trash');
+		$this->unregisterTask('report');
+		$this->unregisterTask('orderup');
+		$this->unregisterTask('orderdown');
+		$this->unregisterTask('delete');
+		$this->unregisterTask('publish');
+		$this->unregisterTask('reorder');
+		$this->unregisterTask('saveorder');
+		$this->unregisterTask('checkin');
+		$this->unregisterTask('saveOrderAjax');
+		$this->unregisterTask('runTransition');
+	}
+
+	public function getModel($name = 'Exporttrails', $prefix = 'Administrator', $config = ['ignore_request' => true])
+	{
+		return parent::getModel($name, $prefix, $config);
+	}
+}
---
+++ component/backend/tmpl/exporttrails/emptystate.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package   AkeebaDataCompliance
+ * @copyright Copyright (c)2018-2021 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Layout\LayoutHelper;
+
+$displayData = [
+	'textPrefix' => 'COM_DATACOMPLIANCE_EXPORTTRAILS',
+	'formURL'    => 'index.php?option=com_datacompliance&view=exporttrails',
+	//'helpURL'    => '',
+	'icon'       => 'fa fa-file-export',
+	//'createURL'  => '',
+];
+
+echo LayoutHelper::render('joomla.content.emptystate', $displayData);
