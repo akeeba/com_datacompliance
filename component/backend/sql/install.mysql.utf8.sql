1883735c1f2f767c1bd2871de39a6d77a3dd783a
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2021-12-06 11:33:59 +0200

    Joomla's database installation management is broken LOL

--- component/backend/sql/install.mysql.utf8.sql
+++ component/backend/sql/install.mysql.utf8.sql
@@ -4,8 +4,7 @@
  * @license   GNU General Public License version 3, or later
  */
 
-CREATE TABLE IF NOT EXISTS `#__datacompliance_exporttrails`
-(
+CREATE TABLE IF NOT EXISTS `#__datacompliance_exporttrails` (
     `datacompliance_exporttrail_id` bigint(20)          NOT NULL AUTO_INCREMENT,
     `user_id`                       bigint(20) unsigned NOT NULL,
     `created_on`                    datetime            NOT NULL,
@@ -13,11 +12,10 @@ CREATE TABLE IF NOT EXISTS `#__datacompliance_exporttrails`
     `requester_ip`                  varchar(255)        NOT NULL,
     PRIMARY KEY (`datacompliance_exporttrail_id`),
     KEY `#__datacompliance_exporttrail_user` (`user_id`)
-) DEFAULT COLLATE utf8_general_ci;
+) DEFAULT COLLATE utf8mb4_unicode_ci;
 
 
-CREATE TABLE IF NOT EXISTS `#__datacompliance_wipetrails`
-(
+CREATE TABLE IF NOT EXISTS `#__datacompliance_wipetrails` (
     `datacompliance_wipetrail_id` BIGINT(20)                        NOT NULL AUTO_INCREMENT,
     `user_id`                     bigint(20)                        NOT NULL,
     `type`                        enum ('lifecycle','user','admin') NOT NULL DEFAULT 'user',
@@ -26,20 +24,18 @@ CREATE TABLE IF NOT EXISTS `#__datacompliance_wipetrails`
     `requester_ip`                varchar(255)                      NOT NULL,
     `items`                       longtext,
     PRIMARY KEY (`datacompliance_wipetrail_id`)
-) DEFAULT COLLATE utf8_general_ci;
+) DEFAULT COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `#__datacompliance_consenttrails`
-(
+CREATE TABLE IF NOT EXISTS `#__datacompliance_consenttrails` (
     `created_on`   datetime     NOT NULL,
     `created_by`   bigint(20)   NOT NULL,
     `requester_ip` varchar(255) NOT NULL,
     `enabled`      int(1)       NOT NULL DEFAULT 0,
     PRIMARY KEY (`created_by`)
-) DEFAULT COLLATE utf8_general_ci;
+) DEFAULT COLLATE utf8mb4_unicode_ci;
 
 
-CREATE TABLE IF NOT EXISTS `#__datacompliance_usertrails`
-(
+CREATE TABLE IF NOT EXISTS `#__datacompliance_usertrails` (
     `datacompliance_usertrail_id` BIGINT(20)   NOT NULL AUTO_INCREMENT,
     `user_id`                     bigint(20)   NOT NULL,
     `created_on`                  datetime     NOT NULL,
@@ -47,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `#__datacompliance_usertrails`
     `requester_ip`                varchar(255) NOT NULL,
     `items`                       longtext,
     PRIMARY KEY (`datacompliance_usertrail_id`)
-) DEFAULT COLLATE utf8_general_ci;
+) DEFAULT COLLATE utf8mb4_unicode_ci;
 
 DROP TABLE IF EXISTS `#__datacompliance_cookietrails`;
 
